8fcff9ab3bd974d150d43a3fb37ee398b473a69b
Vlad Dubovoy <vladdubovoy@ukr.net>
2020-12-08 13:28:32 +0200

    add header people-talking

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,5 @@ const menu = document.querySelector(".menu-burger");
 
 menu.onclick = function(){
     menu.classList.toggle("menu-burger--open");
-    document.querySelector(".navigation__list").classList.toggle("navigation__list--open");
+    document.querySelector(".navigation").classList.toggle("navigation--open");
 };
