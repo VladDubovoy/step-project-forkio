3fe2fed89b7196d25a553b5a93ca7835bb806153
Vlad Dubovoy <vladdubovoy@ukr.net>
2020-12-11 01:54:57 +0200

    welcome and people-talking changed

--- gulp-tasks/img.js
+++ gulp-tasks/img.js
@@ -1,21 +1,23 @@
 const { src, dest } = require("gulp");
-const imagemin = require('gulp-imagemin');
+const imagemin = require("gulp-imagemin");
 
-const img = () => 
-    src("./src/img/*")
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 80, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false},
-                ]
-            })
-        ], {
-            verbose: true
-        }))
-        .pipe(dest('dist/img'));
+const img = () =>
+  src("./src/img/*")
+    .pipe(
+      imagemin(
+        [
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.mozjpeg({ quality: 90, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
+          imagemin.svgo({
+            plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+          }),
+        ],
+        {
+          verbose: true,
+        }
+      )
+    )
+    .pipe(dest("dist/img"));
 
 exports.img = img;
